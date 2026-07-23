473f522ed2c471f8743e580fe8e595545ba8ea1b
Rakshit <rakshitshenoy0011@gmail.com>
2026-07-23 18:40:43 +0530

    Add comments to make_a_gif2.py for clarity
    Added comments to clarify code functionality.

--- make_a_gif2.py
+++ make_a_gif2.py
@@ -1,9 +1,9 @@
-from pathlib import Path
-import imageio.v3 as iio
+from pathlib import Path#import that helps with the path of the image that we choose
+import imageio.v3 as iio #starting import that helps with importing screenshots aka jpg files from ur own device
 
 base_dir = Path(__file__).resolve().parent
 filenames = [base_dir / 'gif6.jpg', base_dir / 'gif5.jpg']
 images = [iio.imread(filename) for filename in filenames]
 
-iio.imwrite(base_dir / 'meyip.gif', images, duration=500, loop=0)
-print(f'GIF saved to: {base_dir / "meyip.gif"}')
+iio.imwrite(base_dir / 'meyip.gif', images, duration=500, loop=0)#this line shows the duration and how long and how will the imgaes change
+print(f'GIF saved to: {base_dir / "meyip.gif"}')#end
